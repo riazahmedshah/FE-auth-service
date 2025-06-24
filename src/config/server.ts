efc8ac22baae385bd82d046057cc8a85ad112db9
riazahmedshah <riyazsh360@gmail.com>
2025-06-24 14:26:29 +0530

    first commit

---
+++ src/config/server.ts
@@ -0,0 +1,6 @@
+import { app } from "../index"
+
+app.listen(1200,() => {
+    console.log("Server is running on port 1200");
+})
+
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express"
+import { router } from "./routes";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use("/api", router);
+
+
+export {app};
