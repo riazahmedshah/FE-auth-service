89a678ae9698d750491f63be1f35c1676f244d2a
riazahmedshah <riyazsh360@gmail.com>
2025-06-25 12:32:20 +0530

    added signin route

--- src/routes/v1/routes.ts
+++ src/routes/v1/routes.ts
@@ -1,8 +1,9 @@
 import express from "express"
-import { createUser, getUser } from "../../controllers/UserController";
+import { createUser, getUser, signIn } from "../../controllers/UserController";
 
 export const v1AuthRoutes = express.Router();
 
 v1AuthRoutes.post("/user", createUser);
+v1AuthRoutes.post("/signin", signIn);
 v1AuthRoutes.get("/userByEmail", getUser);
 
